e6f02028c87006de0be49176861540c96086860c
YMY15 <86570322+YMY15@users.noreply.github.com>
2025-09-13 22:17:23 +0300

    Update ServicesPage.js

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -65,8 +65,8 @@ const ServicesPage = () => {
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage:
-            "url('/images/Background.jpg')",
+          backgroundImage: "url('/images/Background.jpg')",
+            
         }}
       />
       <div className="absolute inset-0 bg-black bg-opacity-60" />
